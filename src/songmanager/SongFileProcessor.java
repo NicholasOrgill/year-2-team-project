ca58cfdfc257a24aa1f1a6fc8a68aa71c4c9a0da
axk588 <axk588@cs.bham.ac.uk>
2017-02-10 01:04:13 +0000

    Add function to convert XML into song object

--- src/songmanager/SongFileProcessor.java
+++ src/songmanager/SongFileProcessor.java
@@ -1,17 +1,21 @@
 package songmanager;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 public class SongFileProcessor {
 
 	public SongFileProcessor() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -91,6 +95,78 @@ public class SongFileProcessor {
 			xmlOutput.output(doc, new FileWriter(outputPath));
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Reads a game-ready XML file and produces a SongObject
+	 * @param inputPath
+	 */
+	public SongObject ReadSongObjectFromXML(String inputPath) {
+		try {
+			// Create document from file
+			SAXBuilder saxBuilder = new SAXBuilder();
+			File inputFile = new File(inputPath);
+			Document doc = saxBuilder.build(inputFile);
+			
+			// Set up variables for song information
+			String title, artist;
+			int songLength, startBeat, averageTempo;
+			Note[] notes;
+			Beat[] beats;			
+			
+			// Retrieve singular elements
+			Element songElement = doc.getRootElement();
+			title = songElement.getChild("title").getText();
+			artist = songElement.getChild("artist").getText();
+			averageTempo = Integer.valueOf(songElement.getChild("averageTempo").getText());
+			songLength = Integer.valueOf(songElement.getChild("songLength").getText());
+			startBeat = Integer.valueOf(songElement.getChild("startBeat").getText());
+			
+			// Retrieve beats
+			Element beatsElement = songElement.getChild("beats");
+			List<Element> beatElements = beatsElement.getChildren();
+			int beatCount = beatElements.size();
+			beats = new Beat[beatCount];
+			
+			for (int i = 0; i < beatElements.size(); i++) {
+				int time = beatElements.get(i).getAttribute("time").getIntValue();
+				int measure = beatElements.get(i).getAttribute("measure").getIntValue();
+				beats[i] = new Beat(time, measure);
+			}
+			
+			// Retrieve notes
+			Element notesElement = songElement.getChild("notes");
+			List<Element> noteElements = notesElement.getChildren();
+			int noteCount = noteElements.size();
+			notes = new Note[noteCount];
+			
+			for (int i = 0; i < noteElements.size(); i++) {
+				int time = noteElements.get(i).getAttribute("time").getIntValue();
+				int sustain = noteElements.get(i).getAttribute("sustain").getIntValue();
+				
+				ArrayList<Integer> buttonsList = new ArrayList<Integer>();
+				for (int j = 0; j < 4; j++) {
+					int buttonActive = noteElements.get(i).getAttribute("button" + j).getIntValue();
+					if (buttonActive == 1) {
+						buttonsList.add(j);
+					}
+				}
+				
+				int[] buttons = new int[buttonsList.size()];
+				for (int j = 0; j < buttonsList.size(); j++) {
+					buttons[i] = buttonsList.get(i);
+				}
+				
+				notes[i] = new Note(time, sustain, buttons);
+			}
+			
+			// Create and return a SongObject
+			SongObject obj = new SongObject(title, artist, songLength, averageTempo, startBeat, notes, beats);
+			return obj;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
