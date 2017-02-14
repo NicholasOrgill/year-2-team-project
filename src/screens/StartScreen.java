05b83f20200a63584761d162c300a631e91938e8
Bobby Dilley <bobbydilley@Bobbys-Mac-mini.local>
2017-02-14 13:19:34 +0000

    Add screens

--- src/screens/StartScreen.java
+++ src/screens/StartScreen.java
@@ -4,70 +4,53 @@ import java.awt.Graphics;
 
 import engine.GameObject;
 import engine.Screen;
-import sprites.BottomBarSprite;
-import sprites.DotSpriteBackground;
-import sprites.FancyCenterTextSprite;
-import sprites.MenuCenterTextSprite;
-import sprites.TopBarSprite;
+import sprites.ImageSprite;
 import utils.ColorPack;
+import utils.ImageLoader;
 
-public class NetworkWait extends Screen {
-	private TopBarSprite topBarSprite;
-	private BottomBarSprite bottomBarSprite;
-	private DotSpriteBackground dotBackground;
-
-	private MenuCenterTextSprite[] centerText;
-	private FancyCenterTextSprite titleText;
+public class StartScreen extends Screen {
 	int counter = 0;
-	int sel = 0;
-
-	public NetworkWait(GameObject gameObject) {
+	private ImageSprite image;
+	
+	public StartScreen(GameObject gameObject) {
 		super(gameObject);
-		topBarSprite = new TopBarSprite("NETWORK WAIT");
-		bottomBarSprite = new BottomBarSprite();
-		bottomBarSprite.setLeftText("PLEASE WAIT");
-		bottomBarSprite.setRightText("PLEASE WAIT");
+		setNextScreen(new JUScreen(gameObject));
 		
-		titleText = new FancyCenterTextSprite((getScreenWidth() / 2), (int) (getScreenHeight() * 0.4), "BeatNetwork");
-		
-		dotBackground = new DotSpriteBackground(10, 10, 20, 30);
-		dotBackground.setScreenSize(getScreenWidth(), getScreenHeight());
-
+		image = new ImageSprite(getScreenWidth() / 2, (int)(getScreenHeight() * 0.4), ImageLoader.loadImageFromResource("src/res/images/konami_logo.png"));
+		image.setScreenSize(getScreenWidth(), getScreenHeight());
+		image.setSize(0.7f);
+		image.setOpacity(0);
 	}
 
 	@Override
 	public void update() {
-	
-		topBarSprite.setScreenSize(getScreenWidth(), getScreenHeight());
-		topBarSprite.update();
-
-		bottomBarSprite.setScreenSize(getScreenWidth(), getScreenHeight());
-		bottomBarSprite.update();
-
-		titleText.setScreenSize(getScreenWidth(), getScreenHeight());
-		titleText.update();
+		counter++;
+		if(counter == 200) {
+			image.fadeIn();
+			
+		}
 		
-		dotBackground.setScreenSize(getScreenWidth(), getScreenHeight());
-		dotBackground.update();
+		if(counter == 500) {
+			image.impress();
+			image.fadeOut();
+		}
+		if(counter == 600) {
+			moveScreen();
+		}
+		
+		image.setScreenSize(getScreenWidth(), getScreenHeight());
+		image.update();
 		
 	}
 
 	@Override
 	public void draw(Graphics context) {
-
-		dotBackground.draw(context);
-
 		// Draw in the background color
-		context.setColor(ColorPack.PRIMARY);
+		context.setColor(ColorPack.WHITE);
 		context.fillRect(0, 0, getScreenWidth(), getScreenHeight());
-
-		// Draw the top and bottom
-		bottomBarSprite.draw(context);
-		topBarSprite.draw(context);
-
-
-		// Draw the title text
-		titleText.draw(context);
+	
+		image.draw(context);
 	}
+
 
 }
