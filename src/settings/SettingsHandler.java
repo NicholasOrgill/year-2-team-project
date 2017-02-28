77cde89cc074a2fb5b4e5b32dd972e733173d796
axk588 <axk588@cs.bham.ac.uk>
2017-02-28 13:50:05 +0000

    Add skeleton for SettingsHandler

--- src/settings/SettingsHandler.java
+++ src/settings/SettingsHandler.java
@@ -1,9 +1,37 @@
 package settings;
 
+import java.util.HashMap;
+
 public class SettingsHandler {
 
+	private String filePath;
+	
 	public SettingsHandler() {
-		// TODO Auto-generated constructor stub
+		filePath = "settings.xml";
 	}
-
+	
+	/**
+	 * Changes the saved value of a particular setting.
+	 * @param settingName Name of the setting to be changed
+	 * @param newValue New value of the setting (as a string)
+	 */
+	public void changeSetting(String settingName, String newValue) {
+		
+	}
+	
+	/**
+	 * Reads a particular setting from the settings file
+	 * @param settingName Name of the setting to be read
+	 * @return Value of the setting (as a string)
+	 */
+	public String readSetting(String settingName) {
+		return null;
+	}
+	
+	/**
+	 * Returns a HashMap of setting names to setting values for all settings
+	 */
+	public HashMap<String, String> readAllSettings() {
+		return null;
+	}
 }
