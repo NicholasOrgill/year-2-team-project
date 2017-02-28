ae8193fd0f1b3a1419ae5813560e9a66a6d39052
axk588 <axk588@cs.bham.ac.uk>
2017-02-28 14:20:14 +0000

    Add settings read and write functionality

--- src/settings/SettingsHandler.java
+++ src/settings/SettingsHandler.java
@@ -1,23 +1,24 @@
 package settings;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-
-import songmanager.Beat;
-import songmanager.Note;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 public class SettingsHandler {
 
 	private String filePath;
 	
 	public SettingsHandler() {
-		filePath = "settings.xml";
+		filePath = "src/settings/settings.xml";
 	}
 	
 	/**
@@ -39,9 +40,21 @@ public class SettingsHandler {
 			}		
 			Element settingsElement = doc.getRootElement();
 			
+			// Retrieve and change the requested element
+			Element elem = settingsElement.getChild(settingName);
+			elem.setText(newValue);
 			
+			// Output XML back to file
+			XMLOutputter xmlOutput = new XMLOutputter();
+			xmlOutput.setFormat(Format.getPrettyFormat());
+			try {
+				xmlOutput.output(doc, new FileWriter(filePath));
+			} catch (IOException e) {
+				System.out.println("Error writing XML to file: " + e);
+			}
 		} catch (JDOMException e) {
 			System.out.println("Error parsing Settings file with JDOM: " + e);
+			System.exit(1);
 		}
 	}
 	
@@ -51,13 +64,69 @@ public class SettingsHandler {
 	 * @return Value of the setting (as a string)
 	 */
 	public String readSetting(String settingName) {
-		return null;
+		try {
+			// Create document from file
+			SAXBuilder saxBuilder = new SAXBuilder();
+			File file = new File(filePath);
+			Document doc = null;
+			try {
+				doc = saxBuilder.build(file);
+			} catch (IOException e) {
+				System.out.println("Error reading Settings file: " + e);
+				System.exit(1);
+			}		
+			Element settingsElement = doc.getRootElement();
+			
+			// Retrieve the requested element
+			Element elem = settingsElement.getChild(settingName);
+			return elem.getText();
+		} catch (JDOMException e) {
+			System.out.println("Error parsing Settings file with JDOM: " + e);
+			System.exit(1);
+			return null;
+		}
 	}
 	
 	/**
 	 * Returns a HashMap of setting names to setting values for all settings
 	 */
 	public HashMap<String, String> readAllSettings() {
-		return null;
+		try {
+			// Create document from file
+			SAXBuilder saxBuilder = new SAXBuilder();
+			File file = new File(filePath);
+			Document doc = null;
+			try {
+				doc = saxBuilder.build(file);
+			} catch (IOException e) {
+				System.out.println("Error reading Settings file: " + e);
+				System.exit(1);
+			}		
+			Element settingsElement = doc.getRootElement();
+			
+			// Get all children of the element
+			List<Element> elementList = settingsElement.getChildren();
+			
+			// Add names and value of each child to the HashMap and return
+			HashMap<String, String> settings = new HashMap<String, String>();
+			for (int i = 0; i < elementList.size(); i++) {
+				Element elem = elementList.get(i);
+				settings.put(elem.getName(), elem.getText());
+			}
+			return settings;
+		} catch (JDOMException e) {
+			System.out.println("Error parsing Settings file with JDOM: " + e);
+			System.exit(1);
+			return null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		SettingsHandler h = new SettingsHandler();
+		System.out.println(h.readSetting("testSetting"));
+		h.changeSetting("testSetting", "32");
+		System.out.println(h.readSetting("testSetting"));
+		h.changeSetting("testSetting", "68");
+		System.out.println(h.readSetting("testSetting"));
 	}
 }
